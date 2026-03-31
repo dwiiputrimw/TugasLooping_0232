d03c39dd710d47980fed2a68977f3f281eb80a5b
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-31 15:56:10 +0700

    Menambahkaan fungsi tampilMenu

--- Looping.cpp
+++ Looping.cpp
@@ -5,51 +5,16 @@ using namespace std;
 int angka;
 int pilihan;
 
-// cek bilangan prima
-bool cekPrima() {
-    if (angka < 2) return false;
-    int i = 2;
-    while (i * i <= angka) {
-        if (angka % i == 0) return false;
-        i++;
-    }
-    return true;
-}
+// (fungsi lain tetap sama)
 
-// cek bilangan fibonacci
-bool cekFibonacci() {
-    int a = 0, b = 1;
-    while (b < angka) {
-        int temp = b;
-        b = a + b;
-        a = temp;
-    }
-    return (b == angka || angka == 0);
-}
-
-// prosedur input angka
-void inputAngka() {
-    cout << "Masukkan angka: ";
-    cin >> angka;
-    if (angka < 0) {
-        cout << "Input tidak boleh negatif!" << endl;
-    }
-}
-
-// prosedur tampil hasil prima
-void tampilHasilPrima() {
-    if (cekPrima())
-        cout << angka << " adalah bilangan prima." << endl;
-    else
-        cout << angka << " bukan bilangan prima." << endl;
-}
-
-// prosedur tampil hasil fibonacci
-void tampilHasilFibonacci() {
-    if (cekFibonacci())
-        cout << angka << " adalah bilangan fibonacci." << endl;
-    else
-        cout << angka << " bukan bilangan fibonacci." << endl;
+// function tampil menu
+void tampilMenu() {
+    cout << "\n=== MENU ===" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Pilihan: ";
+    cin >> pilihan;
 }
 
 int main() {
